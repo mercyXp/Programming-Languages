2c8535aa83398e810c32cad9a3f0b140873de2a5
MercyXp <munzenzimercy2024@gmail.com>
2025-05-13 17:28:53 +0700

    addition from user input

---
+++ Python/Basics/Level_1/0x02add.py
@@ -0,0 +1,4 @@
+num_1 = float(input("Enter first number: ")) #string by default hence the type conversion
+num_2 = float(input("Enter second number: "))
+
+print(f"Result after adding: {num_1 + num_2}")
