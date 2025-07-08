0a0343606d1bb217654b34a57ddc15ea91d156e2
MercyXp <munzenzimercy2024@gmail.com>
2025-07-08 19:16:35 +0700

    task 5 complete

---
+++ JavaScript/0x00_Practical_Tasks/1.Complete_Beginner/week_1/Day_2/task5.js
@@ -0,0 +1,5 @@
+let age = 24;
+console.log(`I'm ${age} years old`); // or console.log("I'm " + age + " years old");
+
+//Advanced
+//console.log("I'm ".concat(age, " years old")); less commonly used
