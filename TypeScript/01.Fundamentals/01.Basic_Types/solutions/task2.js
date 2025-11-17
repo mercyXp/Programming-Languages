dbdb85d8acc0588e7a0564fbf429c74a52222768
MercyXp <munzenzimercy2024@gmail.com>
2025-11-17 15:57:24 +0200

    number

---
+++ TypeScript/01.Fundamentals/01.Basic_Types/solutions/task2.js
@@ -0,0 +1,4 @@
+var age = 25;
+var price = 19.99;
+console.log('Age:', age);
+console.log('Price:', price);
---
+++ TypeScript/01.Fundamentals/01.Basic_Types/solutions/task2.ts
@@ -0,0 +1,5 @@
+let age: number = 25;
+let price: number = 19.99;
+
+console.log('Age:', age);
+console.log('Price:', price);
